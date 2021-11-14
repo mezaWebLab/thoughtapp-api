43146105c51167948113ce430f3509720fa970de
Alex Meza <mezawebdev@gmail.com>
2021-11-13 16:28:51 -0800

    updatet

--- database/migrations/1629077005025_thoughts.ts
+++ database/migrations/1629077005025_thoughts.ts
@@ -16,7 +16,7 @@ export default class Thoughts extends BaseSchema {
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
        */
       table.timestamp('created_at', { useTz: true })
-      table.timestamp('updated_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true }).defaultTo(this.now())
     })
   }
 
